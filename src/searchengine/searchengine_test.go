7ff2f24a4c7675226caf70c5f9246c552b3990c4
ziad amr shalaby <zezoamr@yandex.com>
2023-09-20 06:32:22 +0300

    Create searchengine_test.go

---
+++ src/searchengine/searchengine_test.go
@@ -0,0 +1,82 @@
+package searchengine
+
+import (
+	"errors"
+	"io"
+	"testing"
+	"time"
+
+	"github.com/zezoamr/golang-searchengine/parsing"
+)
+
+func TestCrawlPage(t *testing.T) {
+	typedClient, client, err := getClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	deleteIndex(typedClient, "test")
+	err = createIndex(typedClient, "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pages := []parsing.Page{
+		{
+			Url:   "https://www.google.com",
+			Words: "google google google",
+			Links: []string{
+				"https://www.google.com",
+				"https://www.google.com/?q=hello",
+			},
+			Err: errors.New(""),
+		},
+		{
+			Url:   "https://www.facebook.com",
+			Words: "facebook google",
+			Links: []string{
+				"https://www.facebook.com",
+				"https://www.facebook.com/signup",
+			},
+			Err: errors.New(""),
+		},
+	}
+
+	resp, err := createDocuments(typedClient, "test", pages)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf(string(body))
+
+	time.Sleep(5 * time.Second) //give time for documents to be available to be searched
+
+	resp, err = search(client, "test", "google", 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err = io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf(string(body))
+
+	resp, err = search(client, "test", "google", 3)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err = io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf(string(body))
+
+}
