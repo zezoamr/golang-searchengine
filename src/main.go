ac6f17687656609798f27a80dc421aef683ea9b3
ziad amr shalaby <zezoamr@yandex.com>
2023-09-22 15:55:35 +0300

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,29 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"net/http/httptest"
-
-	"github.com/zezoamr/golang-searchengine/parsing"
+	"github.com/zezoamr/golang-searchengine/api"
 )
 
 func main() {
-	crawlLinks := []string{"https://en.wikipedia.org/wiki/Supersampling"}
-
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		html := "<html><body><p>Paragraph 1</p><p>Paragraph 2</p></body></html>"
-		w.Write([]byte(html))
-	}))
-	defer server.Close()
-	// crawlLinks := []string{server.URL}
-	p := parsing.Crawl(crawlLinks, 4, 2, 2, 1)
-
-	if len(p) > 0 {
-		fmt.Println(len(p))
-		//fmt.Println(p[0].Links)
-		//fmt.Println(p[0].Words)
-	} else {
-		fmt.Println("No pages were crawled.")
-	}
+	api.Api()
 }
